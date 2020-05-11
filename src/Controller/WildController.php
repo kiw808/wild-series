9d9e249ec106a9b2ff33c319ca4ddc9fdf4ab7ae
Pierre Muller <pierremanu.muller@gmail.com>
2020-05-11 18:09:36 +0200

    show slug

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -4,8 +4,8 @@
 namespace App\Controller;
 
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WildController extends AbstractController
@@ -18,6 +18,29 @@ class WildController extends AbstractController
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Series',
+        ]);
+    }
+
+    /**
+     * @Route("/wild/show/{slug}",
+     *     requirements={"slug"="[a-z0-9-]+"},
+     *     defaults={"slug"="none"},
+     *     name="wild_show"
+     * )
+     * @param string $slug
+     * @return Response
+     */
+    public function show(string $slug) :Response
+    {
+        if ($slug != 'none') {
+            $slugTitle = str_replace('-', ' ', $slug);
+            $slugTitle = ucwords($slugTitle);
+        } else {
+            $slugTitle = 'Aucune série sélectionnée, veuillez choisir une série';
+        }
+
+        return $this->render('wild/show.html.twig', [
+            'slugTitle' => $slugTitle
         ]);
     }
 }
