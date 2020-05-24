b535ef47a5b1646173c9dd961b4409f5f116d5e3
Pierre Muller <pierremanu.muller@gmail.com>
2020-05-24 18:35:58 +0200

    program view

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Program;
+use App\Entity\Season;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -112,6 +113,54 @@ class WildController extends AbstractController
         return $this->render('wild/category.html.twig', [
             'category' => $category,
             'programs' => $programs,
+        ]);
+    }
+
+    /**
+     * @Route("/program/{slug<^[a-z0-9-]+$>}",
+     *     defaults={"slug" = null},
+     *     name="program"
+     * )
+     * @param string|null $slug
+     * @return Response
+     */
+    public function showByProgram(?string $slug) :Response
+    {
+        if (!$slug) {
+            throw $this->createNotFoundException(
+                'No slug has been sent to find a program in program\'s table .'
+            );
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ',
+            ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with ' . $slug . ' title, found in program\'s table .'
+            );
+        }
+
+        $seasons = $this->getDoctrine()
+            ->getRepository(Season::class)
+            ->findAll();
+
+        if (!$seasons) {
+            throw $this->createNotFoundException(
+                'No season found in ' . $slug . ' program\'s table'
+            );
+        }
+
+        return $this->render('wild/program.html.twig', [
+            'program' => $program,
+            'seasons' => $seasons,
         ]);
     }
 }
